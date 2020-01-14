a123ab371810c7bf444b586a3ef8c4606bc0652a
viskoos <potniye@gmail.com>
2020-01-14 23:48:27 +0600

    Updates

---
+++ precache-manifest.8f767d02a4b4457b65e132744bab61bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73e5902251749a6559a625c69ad203d",
+    "url": "/smokeCounter/index.html"
+  },
+  {
+    "revision": "9051e9214743de7ebfe4",
+    "url": "/smokeCounter/static/css/main.13e7342e.chunk.css"
+  },
+  {
+    "revision": "f15d1a546f73374bc93c",
+    "url": "/smokeCounter/static/js/2.0dcc1b54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smokeCounter/static/js/2.0dcc1b54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9051e9214743de7ebfe4",
+    "url": "/smokeCounter/static/js/main.356cbbea.chunk.js"
+  },
+  {
+    "revision": "35d996b4060fa3f021a7",
+    "url": "/smokeCounter/static/js/runtime-main.a343912d.js"
+  }
+]);
